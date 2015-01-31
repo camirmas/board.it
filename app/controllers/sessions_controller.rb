045156fb9a48eb82dff12158e2d79dc5a1b1fb91
Cameron Irmas <cjirmas@gmail.com>
2015-01-31 13:45:14 -0800

    users can sign in with reddit account

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.from_omniauth(user_params)
+    session[:access_token] = request.env['omniauth.auth']['credentials']['token']
+    session[:access_token_secret] = request.env['omniauth.auth']['credentials']['secret']
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    User.from_omniauth(env["omniauth.auth"])
+  end
+end
