001132d922b07fbf4c289096bed033811b0f1fee
Cameron Irmas <cjirmas@gmail.com>
2015-02-02 14:07:34 -0800

    update to avoid merge conflict

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
   var url = "http://www.reddit.com";
   var jsonP = ".json?jsonp=?";
-
-  $.ajax(url + '/u/cjirmas/' + jsonP, {
-    type: 'get',
-    dataType: 'jsonp'
-  }).done(function(data) {
-    // console.log(data);
-    for(var i = 0; i < data.data.children.length; i++) {
-      var childData = data.data.children[i].data;
-      // console.log(childData.title);
-    }
-  });
 });
