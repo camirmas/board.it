2fc56ecd5698553f50a77c5fecea7c2bfff7ec6b
msmykowski <smycal@gmail.com>
2015-02-02 10:30:05 -0800

    added subreddit option on frontpage

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -27,4 +27,56 @@ $(document).ready(function() {
       };
     });
   });
+
+  var inputVal = $('input').val()
+
+  $('input').on('keyup', function(e) {
+    if (e.keyCode == 13) {
+      $.ajax(url + "/r/" + $('input').val() + ".json?limit=100&count=50", {
+        type: 'get',
+      }).done(function(data) {
+        console.log(data);
+        $('ul').replaceWith($('<ul>' + data.data.children[0].data.title + '</ul>'));
+        $('img').replaceWith($('<img class="ui centered rounded bordered image" src =' + data.data.children[0].data.url + '>'));
+        var i = 0
+
+        $(document).on('keyup', function(e) {
+          console.log('hi');
+          var childData = data.data.children[i].data;
+          if (e.keyCode == 40 && i < 99) {
+            i += 1;
+            childData = data.data.children[i].data;
+            $('ul').replaceWith($('<ul>' + childData.title + '</ul>'));
+            $('img').replaceWith($('<img class="ui centered rounded bordered image" src =' + childData.url + '>'));
+          }
+          else if (e.keyCode == 38 && i > 0) {
+            i -= 1;
+            childData = data.data.children[i].data;
+            $('ul').replaceWith($('<ul>' + childData.title + '</ul>'));
+            $('img').replaceWith($('<img class="ui centered rounded bordered image" src =' + childData.url + '>'));
+          };
+        });
+      });
+    }
+  });
+
 });
+
+function arrowUpDown(data, counter) {
+  $(document).on('keyup', function(e) {
+    console.log('hi');
+    var childData = data.data.children[counter].data;
+    if (e.keyCode == 40 && counter < 99) {
+      counter += 1;
+      childData = data.data.children[counter].data;
+      $('ul').replaceWith($('<ul>' + childData.title + '</ul>'));
+      $('img').replaceWith($('<img class="ui centered rounded bordered image" src =' + childData.url + '>'));
+    }
+    else if (e.keyCode == 38 && counter > 0) {
+      counter -= 1;
+      childData = data.data.children[counter].data;
+      $('ul').replaceWith($('<ul>' + childData.title + '</ul>'));
+      $('img').replaceWith($('<img class="ui centered rounded bordered image" src =' + childData.url + '>'));
+    };
+  });
+}
