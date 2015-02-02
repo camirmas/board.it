b0c2647e910824853ea5831454f38629846cc9fe
msmykowski <smycal@gmail.com>
2015-02-02 09:50:51 -0800

    front page working with image media types

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,14 +2,29 @@ $(document).ready(function() {
   var url = "http://www.reddit.com";
   var jsonP = ".json?jsonp=?";
 
-  $.ajax(url + '/u/cjirmas/' + jsonP, {
+  $.ajax(url + "/r/pics.json?limit=100&count=50", {
     type: 'get',
-    dataType: 'jsonp'
   }).done(function(data) {
     console.log(data);
-    for(var i = 0; i < data.data.children.length; i++) {
+    ($('<ul>' + data.data.children[0].data.title + '</ul>')).appendTo('.caption');
+    ($('<img class="ui centered rounded bordered image" src =' + data.data.children[0].data.url + '>')).appendTo('.post');
+    var i = 0
+
+    $(document).on('keyup', function(e) {
+      console.log('hi');
       var childData = data.data.children[i].data;
-      console.log(childData.title);
-    }
+      if (e.keyCode == 40 && i < 99) {
+        i += 1;
+        childData = data.data.children[i].data;
+        $('ul').replaceWith($('<ul>' + childData.title + '</ul>'));
+        $('img').replaceWith($('<img class="ui centered rounded bordered image" src =' + childData.url + '>'));
+      }
+      else if (e.keyCode == 38 && i > 0) {
+        i -= 1;
+        childData = data.data.children[i].data;
+        $('ul').replaceWith($('<ul>' + childData.title + '</ul>'));
+        $('img').replaceWith($('<img class="ui centered rounded bordered image" src =' + childData.url + '>'));
+      };
+    });
   });
 });
